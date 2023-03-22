7ed8c4782df0bfeb796795802bbe7374880c7766
Robert Johansson <robert.johansson@ohb-sweden.se>
2023-03-22 16:47:50 +0100

    Inital commit

---
+++ Navigation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Navigation.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        //https://devblogs.microsoft.com/dotnet/announcing-the-dotnet-community-toolkit-800/
+
+        [ObservableProperty]
+        private string? _firstName = "Robert";
+
+        [ObservableProperty]
+        private string? _lastName = "Johansson";
+
+
+        [RelayCommand]
+        private async void Click()
+        {
+            await Task.Delay(1_000);
+           
+            FirstName = "Dennis";
+
+        }
+    }
+}
---
+++ Navigation/ViewModels/ProductViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Navigation.ViewModels
+{
+    public partial class ProductItemViewModel : ObservableObject
+    {
+
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public ProductItemViewModel(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+    }
+
+    public class ProductViewModel : ObservableObject
+    {
+        private readonly ObservableCollection<ProductItemViewModel> _products;
+
+        public ObservableCollection<ProductItemViewModel> Products => _products;
+
+        public ProductViewModel()
+        {
+            _products = new ObservableCollection<ProductItemViewModel>()
+            {
+                new ProductItemViewModel("Grip Tape", 9.99m),
+                new ProductItemViewModel("Deck", 9.99m),
+                new ProductItemViewModel("Wheels", 9.99m),
+                new ProductItemViewModel("Bearings", 9.99m),
+                new ProductItemViewModel("Trucks", 9.99m)
+            };
+        }
+    }
+}
